4e6461b4bda089a45a1cda6aae31fefa1e1be72c
josephndegwa <kariukindegwa98@gmail.com>
2022-09-14 00:33:14 +0300

    initial commit

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Calculator myCalculator = new Calculator();
+        myCalculator.init();
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+import java.util.Scanner;
+
+public class Calculator extends JFrame{
+
+    public void init() {
+        System.out.println("Hello, World!");
+        int n1;
+        int n2;
+        int n3;
+        int cal;
+        int quotient;
+        int remainder;
+
+        System.out.println("Enter two numbers: ");
+        Scanner r = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+    }
+    
+}
